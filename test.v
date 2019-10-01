c7561f55e6167e6917be56425eb310c37de01659
onokatio <onokatio@gmail.com>
2019-10-01 13:39:05 +0900

    fix

--- test.v
+++ test.v
@@ -11,14 +11,13 @@ NOT1  new_not  (a, not_out);
 
 initial begin
    $dumpfile("nand2.vcd");
-   /*$dumpvars(0, TEST_NAND2);*/
+   $dumpvars(0, TEST_NAND2);
    $monitor ("%t: a = %b, b = %b, nand_out = %b", $time, a, b, nand_out);
 
-   a = 0; b = 0;
+        a = 0; b = 0;
    #10  a = 1;
    #10  a = 0; b = 1;
    #10  a = 1;
-
    #10  $finish;
 end
 
