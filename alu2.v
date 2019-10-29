44dae467ee43f6e9b59c486baed561091a37ca6a
onokatio <onokatio@gmail.com>
2019-10-29 23:44:47 +0900

    fix

--- alu2.v
+++ alu2.v
@@ -11,26 +11,26 @@ module ALU (
 
 	DECODER3 opecode_decoder (Mode[0], Mode[1], Mode[2], decoder_x[7:0]);
 
-	// opcode
-	// 0x00 : plus
-	// 0x01 : AND
-	// 0x02 : OR
-	// 0x03 : ExOR
-	// 0x04 : Not ExOR
+	// mode
+	// 0x00 : 000 : plus
+	// 0x01 : 001 : AND
+	// 0x02 : 010 : OR
+	// 0x03 : 011 : ExOR
+	// 0x04 : 100 : Not ExOR
 
-	AND2 set_1 (Ci1, decoder_x[0], Ci2);
-	OR2  set_0_for_or (Ci2, decoder_x[2], Ci3);
-	OR2  set_0_for_exorbar (Ci3, decoder_x[4], Ci4);
+	AND2 set_1_when_mode000 (C_in, decoder_x[0], C_in2);
+	OR2  set_0_when_mode010 (C_in2, decoder_x[2], C_in3);
+	OR2  set_0_when_mode100 (C_in3, decoder_x[4], C_in4);
 
-	FA2 new_fa (A, B, Ci4, So, Cfo);
+	FA2 new_fa (A, B, C_in4, FA_out_s, FA_out_c);
 
-	OR3 new_or_for_ds_1 (Mode[0], Mode[3], Mode[4], dsi1);
-	NOT1 new_not_for_ds (dsi1, dsi2);
-	OR2 new_or_for_ds_2 (Mode[1], Mode[2], dsi3);
-	OR2 new_or_for_ds_3 (dsi2, dsi3, dsi4);
+	NOR3 out_port_check_1 (decoder_x[0], decoder_x[3], decoder_x[4], use_s_as_out);
+	OR2  out_port_check_2 (decoder_x[1], decoder_x[2], use_c_as_out);
 
-	DATASELECTOR2 output_ds (dsi4, Cfo, So, Co);
+	OR2 new_or_for_ds_3 (use_s_as_out, use_c_as_out, Selector);
 
-	AND2 check_c (Cfo, Mode[0], X);
+	DATASELECTOR2 output_ds (Selector, FA_out_s, FA_out_c, X);
+
+	AND2 check_c (FA_out_c, decoder_x[0], C_out);
 
 endmodule
