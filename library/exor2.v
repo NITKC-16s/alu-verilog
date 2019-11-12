5f016cab66dd4d7b1c9eb7990f8e944a6291a2cf
onokatio <onokatio@gmail.com>
2019-11-12 13:28:51 +0900

    fix exor

--- library/exor2.v
+++ library/exor2.v
@@ -8,9 +8,8 @@ module EXOR2 (
 	wire D;
 	wire E;
 	KATIO_NAND2 new_nand_1 (A, B, C);
-	KATIO_NAND2 new_nand_2 (A, B, C);
-	KATIO_NAND2 new_nand_3 (A, C, D);
-	KATIO_NAND2 new_nand_4 (B, C, E);
-	KATIO_NAND2 new_nand_5 (D, E, X);
+	KATIO_NAND2 new_nand_2 (A, C, D);
+	KATIO_NAND2 new_nand_3 (B, C, E);
+	KATIO_NAND2 new_nand_4 (D, E, X);
 
 endmodule
