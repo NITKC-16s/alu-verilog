d37107879784edbe2ef9dd4d02fe1e7b763c89f9
onokatio <onokatio@gmail.com>
2019-11-19 10:59:50 +0900

    better fa implemetation

--- library/fa2.v
+++ library/fa2.v
@@ -4,14 +4,20 @@ module FA2 (
 	input B,
 	input X,
 	output S,
-	output C);
+	output Co);
 
 	wire SS;
 	wire CC;
 	wire CCC;
 
-	HA2 new_ha_1 (A, B, SS, CC);
-	HA2 new_ha_2 (X, SS, S, CCC);
-	KATIO_OR2 new_or (CC, CCC, C);
+	KATIO_NAND2 new_nand_1 (A, B, C);
+	KATIO_NAND2 new_nand_2 (A, C, AC);
+	KATIO_NAND2 new_nand_3 (B, C, BC);
+	KATIO_NAND2 new_nand_4 (AC, BC, ABC);
+	KATIO_NAND2 new_nand_5 (ABC, X, ABCX);
+	KATIO_NAND2 new_nand_6 (ABCX, ABC, S1);
+	KATIO_NAND2 new_nand_7 (ABCX, X , S2);
+	KATIO_NAND2 new_nand_8 (S1, S2 , S);
+	KATIO_NAND2 new_nand_9 (ABCX, C , Co);
 
 endmodule
